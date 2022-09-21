bb4a92230d1537882cfe73a27f1e48ce12dda180
imnotkind <imnotkind307@gmail.com>
2022-09-21 20:11:32 +0900

    Update update_setting.sh

--- update_setting.sh
+++ update_setting.sh
@@ -8,46 +8,35 @@ first=0
 nfs=1
 
 
-echo "fewtask update"
-if [ $first ]
+if [ $first -eq 1 ]
 then
         cd $root
         git clone git@github.com:namsan96/fewtask.git
+        git clone git@github.com:namsan96/metaworld.git
+        git clone git@github.com:namsan96/SiMPL.git -b fewtask
 fi
+
+
 cd $root/fewtask
 git pull
 pip install -e .
-
-echo "metaworld update"
-if [ $first ]
-then
-        cd $root
-        git clone git@github.com:namsan96/metaworld.git
-fi
 cd $root/metaworld
 git pull
 pip install -e .
-
-
-echo "simpl update"
-if [ $first ]
-then
-        cd $root
-        git clone git@github.com:namsan96/SiMPL.git -b fewtask
-fi
 cd $root/SiMPL
 git pull origin fewtask
 pip install -e .
 
 
 
-if [ $nfs ]
+if [ $nfs -eq 1 ]
 then
         ln -s /input/rl /root/nfs
+        
+        pip install -r /root/nfs/requirements.txt
         sudo apt update
         sudo apt install -y libosmesa6-dev libgl1-mesa-glx libglfw3 patchelf
         sudo ln -s /usr/lib/x86_64-linux-gnu/libGL.so.1 /usr/lib/x86_64-linux-gnu/libGL.so
         mkdir /root/.mujoco && cp /root/nfs/mujoco210-linux-x86_64.tar.gz /root/.mujoco && cd /root/.mujoco && tar zxf mujoco210-linux-x86_64.tar.gz
         sed -i "s|x86_64-linux-gnu|x86_64-linux-gnu:/root/.mujoco/mujoco210/bin|" /root/.profile
-        /etc/init.d/jupyter restart
 fi
