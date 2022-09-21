7a673fb41058b70c73463c8e0314e0a687d5fedc
imnotkind <imnotkind307@gmail.com>
2022-09-21 17:16:29 +0900

    add nfs

--- update_setting.sh
+++ update_setting.sh
@@ -4,7 +4,8 @@ conda activate metaworld
 
 
 root="$(pwd)"
-first=1
+first=0
+nfs=1
 
 
 echo "fewtask update"
@@ -37,3 +38,16 @@ fi
 cd $root/SiMPL
 git pull origin fewtask
 pip install -e .
+
+
+
+if [ $nfs ]
+then
+        ln -s /input/rl /root/nfs
+        sudo apt update
+        sudo apt install -y libosmesa6-dev libgl1-mesa-glx libglfw3 patchelf
+        sudo ln -s /usr/lib/x86_64-linux-gnu/libGL.so.1 /usr/lib/x86_64-linux-gnu/libGL.so
+        mkdir /root/.mujoco && cp /root/nfs/mujoco210-linux-x86_64.tar.gz /root/.mujoco && cd /root/.mujoco && tar zxf mujoco210-linux-x86_64.tar.gz
+        sed "s|x86_64-linux-gnu|x86_64-linux-gnu:/root/.mujoco/mujoco210/bin|" /root/.profile
+        /etc/init.d/jupyter restart
+fi
